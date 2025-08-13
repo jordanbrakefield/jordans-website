cc89a23a5b8f61e689acb846cf7fc635f8b35ee5
brake <brak3field@gmail.com>
2025-08-12 18:04:40 -0700

    Added UI elements to Contact form

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,11 +4,6 @@ export default function About() {
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-100),white)] opacity-20" />
       <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl ring-1 shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
-        <img
-          alt=""
-          src="https://tailwindcss.com/plus-assets/img/logos/workcation-logo-indigo-600.svg"
-          className="mx-auto h-12"
-        />
         <figure className="mt-10">
           <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
             <p>
